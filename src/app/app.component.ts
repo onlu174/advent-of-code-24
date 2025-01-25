6200d4878e15dcbff09b5a0622f027e474e9a250
artwall <artur.walentyn@iteo.com>
2025-01-25 12:06:17 +0100

    Creates PuzzleComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,9 @@
-import { Component, signal } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { solveDay1Puzzle } from './puzzle-solutions/day-1-puzzle-solution/day-1-puzzle-solution';
+import { Component } from '@angular/core';
+import { PuzzleComponent } from './components/puzzle/puzzle.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [PuzzleComponent],
 })
-export class AppComponent {
-  private readonly puzzleInputControl = new FormControl<File | null>(null, [Validators.required]);
-
-  puzzleSolution = signal<number | null>(null);
-
-  onFileInputChange(event: Event): void {
-    const input = event.target as HTMLInputElement | null;
-
-    if (input === null) {
-      throw new Error('Input Element is null');
-    }
-
-    const inputFiles = input.files;
-
-    if (inputFiles === null) {
-      throw new Error('Input Element FileList is null');
-    }
-
-    this.puzzleInputControl.setValue(inputFiles.item(0));
-  }
-
-  onFormSubmit(): void {
-    const puzzleInputFile = this.puzzleInputControl.value;
-
-    if (puzzleInputFile === null) return;
-
-    this.solvePuzzle(puzzleInputFile);
-  }
-
-  private solvePuzzle(puzzleInputFile: File): void {
-    const fileReader = new FileReader();
-
-    fileReader.onload = () => {
-      const puzzleInput = fileReader.result as string;
-      const puzzleSolution = solveDay1Puzzle(puzzleInput);
-
-      this.puzzleSolution.set(puzzleSolution);
-    };
-    fileReader.readAsText(puzzleInputFile);
-  }
-}
+export class AppComponent {}
---
+++ src/app/components/puzzle/puzzle.component.ts
@@ -0,0 +1,50 @@
+import { Component, signal } from '@angular/core';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
+import { solveDay1Puzzle } from '../../puzzle-solutions/day-1-puzzle-solution/day-1-puzzle-solution';
+
+@Component({
+  selector: 'app-puzzle',
+  imports: [FormsModule],
+  templateUrl: './puzzle.component.html',
+})
+export class PuzzleComponent {
+  private readonly puzzleInputControl = new FormControl<File | null>(null, [Validators.required]);
+
+  puzzleSolution = signal<number | null>(null);
+
+  onFileInputChange(event: Event): void {
+    const input = event.target as HTMLInputElement | null;
+
+    if (input === null) {
+      throw new Error('Input Element is null');
+    }
+
+    const inputFiles = input.files;
+
+    if (inputFiles === null) {
+      throw new Error('Input Element FileList is null');
+    }
+
+    this.puzzleInputControl.setValue(inputFiles.item(0));
+  }
+
+  onFormSubmit(): void {
+    const puzzleInputFile = this.puzzleInputControl.value;
+
+    if (puzzleInputFile === null) return;
+
+    this.solvePuzzle(puzzleInputFile);
+  }
+
+  private solvePuzzle(puzzleInputFile: File): void {
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      const puzzleInput = fileReader.result as string;
+      const puzzleSolution = solveDay1Puzzle(puzzleInput);
+
+      this.puzzleSolution.set(puzzleSolution);
+    };
+    fileReader.readAsText(puzzleInputFile);
+  }
+}
