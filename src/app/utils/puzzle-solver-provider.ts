d96ecb87885c65dfe90b083a4ce34a618c3e8144
artwall <artur.walentyn@iteo.com>
2025-02-05 18:32:31 +0100

    Creates RouteParam enum

--- src/app/utils/puzzle-solver-provider.ts
+++ src/app/utils/puzzle-solver-provider.ts
@@ -1,9 +1,10 @@
 import { PuzzleSolverService } from '../model/puzzle-solver.service';
 import { Router } from '@angular/router';
 import { CalendarService } from '../services/calendar/calendar.service';
+import { RouteParam } from './route-param';
 
 const puzzleSolverFactory = (router: Router, calendarService: CalendarService): PuzzleSolverService => {
-  const dayParam = router.routerState.snapshot.root.children[0].params['day'];
+  const dayParam = router.routerState.snapshot.root.children[0].params[RouteParam.puzzleDay];
   const dayNumber = Number.parseInt(dayParam);
   const dayIndex = dayNumber - 1;
   const day = calendarService.calendar[dayIndex];
---
+++ src/app/utils/route-param.ts
@@ -0,0 +1,3 @@
+export enum RouteParam {
+  puzzleDay = 'day',
+}
