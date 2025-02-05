3680f4cf7db2f878e2813f04d0b757729861aa91
artwall <artur.walentyn@iteo.com>
2025-02-05 18:16:23 +0100

    Creates Path enum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,20 @@ import { Routes } from '@angular/router';
 import { PuzzleComponent } from './components/puzzle/puzzle.component';
 import { puzzleSolverProvider } from './utils/puzzle-solver-provider';
 import { CalendarComponent } from './components/calendar/calendar.component';
+import { Path } from './utils/path';
 
 export const routes: Routes = [
   {
-    path: 'calendar',
+    path: Path.calendar,
     component: CalendarComponent,
   },
   {
-    path: 'calendar/:day',
+    path: `${Path.calendar}/:day`,
     component: PuzzleComponent,
     providers: [puzzleSolverProvider],
   },
   {
     path: '**',
-    redirectTo: 'calendar',
+    redirectTo: Path.calendar,
   },
 ];
