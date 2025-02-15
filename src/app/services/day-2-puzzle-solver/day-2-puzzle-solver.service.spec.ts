8852e9ed3353ea0662259bf950151b5bd9b1d5eb
artwall <artur.walentyn@iteo.com>
2025-02-15 21:48:37 +0100

    Implements solvePartOne method of Day2PuzzleSolverService

--- src/app/services/day-2-puzzle-solver/day-2-puzzle-solver.service.spec.ts
+++ src/app/services/day-2-puzzle-solver/day-2-puzzle-solver.service.spec.ts
@@ -13,4 +13,20 @@ describe('Day2PuzzleSolverService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('solvePartOne', () => {
+    it('should return correct result', () => {
+      const input = '7 6 4 2 1\n' + '1 2 7 8 9\n' + '9 7 6 2 1\n' + '1 3 2 4 5\n' + '8 6 4 4 1\n' + '1 3 6 7 9';
+
+      expect(service.solvePartOne(input)).toBe(2);
+    });
+  });
+
+  describe('solvePartTwo', () => {
+    xit('should return correct result', () => {
+      const input = '7 6 4 2 1\n' + '1 2 7 8 9\n' + '9 7 6 2 1\n' + '1 3 2 4 5\n' + '8 6 4 4 1\n' + '1 3 6 7 9';
+
+      // expect(service.solvePartTwo(input)).toBe(31);
+    });
+  });
 });
--- src/app/services/day-2-puzzle-solver/day-2-puzzle-solver.service.ts
+++ src/app/services/day-2-puzzle-solver/day-2-puzzle-solver.service.ts
@@ -1,15 +1,54 @@
 import { Injectable } from '@angular/core';
 import { PuzzleSolverService } from '../../model/puzzle-solver.service';
 
+type UnusualData = Report[];
+type Report = Level[];
+type Level = number;
+
 @Injectable({
   providedIn: 'root',
 })
 export class Day2PuzzleSolverService implements PuzzleSolverService {
   solvePartOne(puzzleInput: string): number {
-    throw new Error('Method not implemented.');
+    const unusualData = this.mapPuzzleInput(puzzleInput);
+
+    return unusualData.reduce(
+      (accumulator, currentValue) => (this.checkIfReportSafe(currentValue) ? accumulator + 1 : accumulator),
+      0,
+    );
+  }
+
+  private checkIfReportSafe(report: Report): boolean {
+    enum LevelsDirection {
+      Increasing,
+      Decreasing,
+    }
+
+    let direction: LevelsDirection | undefined = undefined;
+
+    for (let i = 1; i < report.length; i++) {
+      const currentLevel = report[i];
+      const previousLevel = report[i - 1];
+
+      const levelDifference = currentLevel - previousLevel;
+
+      if (levelDifference === 0) return false;
+      if (Math.abs(levelDifference) >= 4) return false;
+
+      const currentLevelsDirection = levelDifference > 0 ? LevelsDirection.Increasing : LevelsDirection.Decreasing;
+
+      if (direction === undefined) direction = currentLevelsDirection;
+      if (currentLevelsDirection !== direction) return false;
+    }
+
+    return true;
   }
 
   solvePartTwo(puzzleInput: string): number {
     throw new Error('Method not implemented.');
+  }
+
+  private mapPuzzleInput(puzzleInput: string): UnusualData {
+    return puzzleInput.split('\n').map((line) => line.split(' ').map((char) => Number(char)));
   }
 }
