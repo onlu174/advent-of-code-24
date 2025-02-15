68dc7e31ca080251cae9bb144664df21f0016a16
artwall <artur.walentyn@iteo.com>
2025-02-15 20:26:58 +0100

    Updates puzzleSolverProvider and puzzleDayDataProvider usage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { PuzzleDayComponent } from './components/puzzle/puzzle-day.component';
-import { puzzleSolverProvider } from './utils/puzzle-solver-provider';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { Path } from './utils/path';
-import { puzzleDayDataProvider } from './utils/puzzle-day-data-provider';
 
 export const routes: Routes = [
   {
@@ -13,7 +11,6 @@ export const routes: Routes = [
   {
     path: Path.puzzle,
     component: PuzzleDayComponent,
-    providers: [puzzleDayDataProvider, puzzleSolverProvider],
   },
   {
     path: '**',
--- src/app/components/puzzle/puzzle-day.component.ts
+++ src/app/components/puzzle/puzzle-day.component.ts
@@ -3,10 +3,13 @@ import { PuzzleSolverService } from '../../model/puzzle-solver.service';
 import { RouterLink } from '@angular/router';
 import { PUZZLE_DAY_DATA } from '../../model/puzzle-day-data';
 import { PuzzleFormComponent, PuzzlePart } from '../puzzle-form/puzzle-form.component';
+import { puzzleSolverProvider } from '../../utils/puzzle-solver-provider';
+import { puzzleDayDataProvider } from '../../utils/puzzle-day-data-provider';
 
 @Component({
   selector: 'app-puzzle-day',
   imports: [RouterLink, PuzzleFormComponent],
+  providers: [puzzleSolverProvider, puzzleDayDataProvider],
   templateUrl: './puzzle-day.component.html',
 })
 export class PuzzleDayComponent {
