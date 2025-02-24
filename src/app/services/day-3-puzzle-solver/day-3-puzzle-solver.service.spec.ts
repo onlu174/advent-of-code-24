f1af4086fb6a1e5c23c5c815c26108d461e4c5fa
artwall <artur.walentyn@iteo.com>
2025-02-24 20:01:08 +0100

    Adds Day3PuzzleSolverService specs

--- src/app/services/day-3-puzzle-solver/day-3-puzzle-solver.service.spec.ts
+++ src/app/services/day-3-puzzle-solver/day-3-puzzle-solver.service.spec.ts
@@ -13,4 +13,20 @@ describe('Day3PuzzleSolverService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('solvePartOne', () => {
+    it('should return correct result', () => {
+      const input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n';
+
+      expect(service.solvePartOne(input)).toBe(161);
+    });
+  });
+
+  describe('solvePartTwo', () => {
+    xit('should return correct result', () => {
+      const input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n';
+
+      expect(service.solvePartTwo(input)).toBe(expect.anything());
+    });
+  });
 });
